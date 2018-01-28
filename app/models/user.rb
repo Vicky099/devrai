56cbeca4f9ab73a4cdc1dc65457475404a42e5bb
vicky099 <vikky.panmand@gmail.com>
2018-01-28 16:04:04 +0530

    [update] 1. UI fixes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   validates :full_name, presence: true
-  validates :date_of_birth, presence: true
+  #validates :date_of_birth, presence: true
   validates :contact_no, presence: true
   validates :address, presence: true
   validates :city, presence: true
